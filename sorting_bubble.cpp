58a35055710cef754a23101a2cc012a777dcb0d0
ashish-509 <101721265+ashish-509@users.noreply.github.com>
2024-02-14 22:04:30 +0545

    Update sorting_bubble.cpp

--- sorting_bubble.cpp
+++ sorting_bubble.cpp
@@ -9,42 +9,40 @@
 #include<vector>
 using namespace std;
 
-void bubbleSort(vector <int> &v){
+void bubbleSort(vector<int> &v) {
     int n = v.size();
 
-    for(int i=0; i<n-1; i++){
-        bool flag = false; // for optimization in terms of time complexity
-        for(int j=0; j<n-(i+1); j++){
-            if (v[j] > v[j+1]){
-                flag = true; // if thre has been any swap in a pass the flag is set true
-                swap(v[j], v[j+1]);
+    for (int i = 0; i < n - 1; i++) {
+        bool flag = false;
+        for (int j = 0; j < n - (i + 1); j++) {
+            if (v[j] > v[j + 1]) {
+                flag = true; 
+                swap(v[j], v[j + 1]);
             }
-            if (!flag) break; // if the flag is still false (no swap in a pass) then the array is already sorted. So break from the loop.
         }
+        if (!flag) break; 
     }
-    return;
 }
 
-int main()
-{
+int main() {
     int n;
-    cout<<"\nEnter the number of elements : ";
-    cin>>n;
+    cout << "\nEnter the number of elements : ";
+    cin >> n;
 
     vector<int> v(n);
-    cout<<"\nEnter "<<n<<" elements of an array : ";
+    cout << "\nEnter " << n << " elements of an array : ";
 
-    for (int i=0; i<n; i++){
-        cin>>v[i];
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
     }
 
     bubbleSort(v);
 
-    cout<<"\nThe sorted array is : ";
-    for (int i=0; i<n; i++){
-        cout<<v[i]<<" ";
+    cout << "\nThe sorted array is : ";
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
     }
-    cout<<endl; 
+    cout << endl;
 
     return 0;
 }
